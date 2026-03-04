a826b2b296600b71f2f34c2bc8f0f73a6e43359f
tt22oo <rntk25w7rq@privaterelay.appleid.com>
2026-03-04 11:38:29 +0900

    Update handler.go

--- honeypot/core/telnet/session/handler.go
+++ honeypot/core/telnet/session/handler.go
@@ -1,11 +1,15 @@
 package session
 
 import (
+	"fmt"
 	"honeypot/core/config"
 	"honeypot/core/logger"
+
+	"github.com/tt22oo/fakeshell/shell/command"
+	"github.com/tt22oo/fakeshell/shell/parser"
 )
 
-func (s *Session) Handler(cfgs *config.Configs) {
+func (s *Session) Handler(cfgs *config.Config) {
 	defer s.Conn.Close()
 
 	err := s.Login(cfgs)
@@ -14,10 +18,26 @@ func (s *Session) Handler(cfgs *config.Configs) {
 		return
 	}
 
+	shell, err := initShell(cfgs)
+	if err != nil {
+		logger.Error("telnet", err.Error())
+		return
+	}
+	shell.User = s.User
+
 	for {
 		input, err := s.WriteAndRead("# ")
 		if err != nil {
 			logger.Error("telnet", err.Error())
+			return
+		}
+
+		tokens := parser.Input(input)
+		result := command.RunCommands(shell, tokens)
+		_, err = s.Conn.Write([]byte(result))
+		if err != nil {
+			data := fmt.Sprintf("Write Error: %s", err.Error())
+			logger.Error("telnet", data)
 			return
 		}
 
