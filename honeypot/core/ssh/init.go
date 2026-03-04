c294750e1dadc4f6dd7392ced7c2ac46f4cc9aaf
tt22oo <rntk25w7rq@privaterelay.appleid.com>
2026-03-04 11:39:27 +0900

    Update init.go

--- honeypot/core/ssh/init.go
+++ honeypot/core/ssh/init.go
@@ -4,7 +4,7 @@ import (
 	"honeypot/core/config"
 )
 
-func Init(cfgs *config.Configs) {
+func Init(cfgs *config.Config) {
 	if cfgs.SSH.Start {
 		for _, addr := range cfgs.SSH.Addrs {
 			go Listen(cfgs, addr)
