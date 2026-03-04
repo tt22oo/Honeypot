e60d892842657416687056fa12130ca8a1f304ca
tt22oo <rntk25w7rq@privaterelay.appleid.com>
2026-03-04 11:40:23 +0900

    Update handler.go

--- honeypot/core/ssh/session/handler.go
+++ honeypot/core/ssh/session/handler.go
@@ -1,22 +1,41 @@
 package session
 
 import (
+	"fmt"
 	"honeypot/core/logger"
 
 	"github.com/gliderlabs/ssh"
+	"github.com/tt22oo/fakeshell/shell/command"
+	"github.com/tt22oo/fakeshell/shell/parser"
 )
 
-func (c *Config) Handler(s ssh.Session) {
+func (cfgs *Config) Handler(s ssh.Session) {
 	defer s.Close()
 
+	shell, err := cfgs.initShell()
+	if err != nil {
+		logger.Error("ssh", err.Error())
+		return
+	}
+	shell.User = s.User()
 	sessionID := s.Context().Value("sessionID").(string)
+
 	for {
-		cmd, err := WriteAndRead(s, "# ")
+		input, err := WriteAndRead(s, "# ")
 		if err != nil {
 			logger.Error("ssh", err.Error())
 			return
 		}
 
-		logger.Cmd(c.Configs, "ssh", s.RemoteAddr().String(), sessionID, cmd)
+		tokens := parser.Input(input)
+		result := command.RunCommands(shell, tokens)
+		_, err = s.Write([]byte(result))
+		if err != nil {
+			data := fmt.Sprintf("Write Error: %s", err.Error())
+			logger.Error("ssh", data)
+			return
+		}
+
+		logger.Cmd(cfgs.Config, "ssh", s.RemoteAddr().String(), sessionID, input)
 	}
 }
